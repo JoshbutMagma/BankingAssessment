f697000ec1d38855a6404825d9d96febc3e5ea2e
JoshbutMagma <HunteJo@whs.school.nz>
2025-03-31 15:11:18 +1300

    Fourth commit

--- Banking/CustomerInfo.java
+++ Banking/CustomerInfo.java
@@ -10,6 +10,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
 public class CustomerInfo
 {
     //initialising variables.
@@ -18,18 +20,9 @@ public class CustomerInfo
     private int accountNumber;
     private String accountType;
     private double accountBalance;
+    File bankData = new File("BankData.csv");
     Random Random = new Random();
     
-    /** //Info creator with all info
-    public void infoCreator(String accountName, String accountAddress, int accountNumber, String accountType, double accountBalance){
-        this.accountName = accountName;
-        this.accountAddress = accountAddress;
-        this.accountNumber = accountNumber;
-        this.accountType = accountType;
-        this.accountBalance = accountBalance;
-        
-        System.out.println(accountName + ", " + accountAddress + ", " + accountNumber + ", " + accountType + ", " + accountBalance);
-    }*/
     //Info Creator with just name, address, and type
     public void infoCreator(String accountName, String accountAddress, String accountType){
         this.accountName = accountName;
@@ -38,6 +31,57 @@ public class CustomerInfo
         this.accountType = accountType;
         this.accountBalance = 0.0;
         
+        infoPrinter();
+        
+        //This is where the info for the new account is saved to the file
+        try{
+            FileWriter myWriter = new FileWriter(bankData);
+            
+            myWriter.write(accountName + "," + accountAddress + "," + accountNumber + "," + accountType + "," + accountBalance);
+            
+            myWriter.flush();
+            myWriter.close();
+        }catch(IOException e){
+            System.out.println("Unexpected error, please try again");
+        }
+    }
+    
+    //Numerous getters for the various values
+    public void infoPrinter(){
         System.out.println("The account is under the name " + accountName + " who lives at " + accountAddress + ", the bank number is " + accountNumber + ", the account type is " + accountType + ", and the account balance is " + accountBalance);
+    }
+    public String nameGetter(){
+        return(this.accountName);
+    }
+    public String addressGetter(){
+        return(this.accountAddress);
+    }
+    public int numberGetter(){
+        return(this.accountNumber);
+    }
+    public String typeGetter(){
+        return(this.accountType);
+    }
+    public double balanceGetter(){
+        return(this.accountBalance);
+    }
+    
+    //Numerous setters for the various values
+    public void nameSetter(String newName){
+        this.accountName = newName;
+    }
+    public void addressSetter(String newAddress){
+        this.accountAddress = newAddress;
+    }
+    public void balanceSetter(double newBalance){
+        this.accountBalance = newBalance;
+    }
+    //This method allows deposits/withdrawals
+    public void balanceChanger(String direction, double value){
+        if(direction.equals("DEPOSIT")){
+            this.accountBalance = accountBalance + value;
+        } else if(direction.equals("WITHDRAW")){
+            this.accountBalance = accountBalance - value;
+        }
     }
 }
--- Banking/main.java
+++ Banking/main.java
@@ -6,17 +6,35 @@
  * 24/03/25-09/04/25
  * 
  * To do list:
- * Set out framework for ArrayList with Arrays
- * Lay out framework for VIEW, EDIT, MOVE, CLOSE
- * Finish working on /\ as well as finishing OPEN and STOP
+ * Do SOMETHING with the ArrayList
+ * Have the software auto remove old data
+ * fix the thing where opening a new account deletes the old one
+ * Have the software save to file when it closes as opposed to opening
+ * Lay out framework for EDIT, MOVE, CLOSE
+ * Finish working on /\ as well as finishing VIEW, STOP
  */
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
 public class main
 {
+    /**
+     * This method allows me to receive inputs and remove commas from said inputs
+     * This is so that when the info is eventually stored to a file seperated by commas, the datapoints won't be messed up because of any commas
+     */
+    private static String commaCheck(){
+        Scanner Keyboard = new Scanner(System.in);
+        String input;
+        
+        input = Keyboard.nextLine();
+        input = input.replace(",","");
+        
+        return input;
+    }
     //This will be where most of the main banking will be done
     public static void main(String[] args){
         //Initialising file and variables
@@ -24,61 +42,89 @@ public class main
         File bankData = new File("BankData.csv");
         Scanner Keyboard = new Scanner(System.in);
         CustomerInfo TestPerson = new CustomerInfo();
+        ArrayList<String> allAccounts = new ArrayList<String>();
         String mainInput;
         String input;
         boolean stillRunning = true;
         boolean errorBlock;
         boolean rightType;
-        //These 3 likely won't be permanent, but will help me with the process of creating accounts
         String name;
         String address;
         String type = "";
         
+        try{
+            Scanner readInfo = new Scanner(bankData);
+            
+            while(readInfo.hasNextLine()){
+                allAccounts.add(readInfo.nextLine());
+            }
+        }catch(IOException e){
+            System.out.println("Unexpected error, please try again");
+        }
+        
         //This is where the banker will be able to do banking
         System.out.println("Hello! I'll be your virtual banking assistant for today.");
-        while(stillRunning==true){
-            errorBlock = true;
-            rightType = true;
+        while(stillRunning){
+            //this is where the banker will be able to input what they would like the software to do
             System.out.println("What would you like help with today? You can: view or edit an account, move money in or out of an account, open or close and account, or end the software.");
             System.out.println("Valid inputs are: VIEW, EDIT, MOVE, OPEN, CLOSE, STOP");
             System.out.print("I would like to: ");
             mainInput = Keyboard.nextLine();
             mainInput = mainInput.toUpperCase();
             
-            if(mainInput.equals("OPEN")){
-                while(errorBlock==true){
-                    /**
-                     * this will be where new info is created using the method in CustomerInfo
-                     */
-                    System.out.println("You have selected 'open a new account'.");
-                    
-                    System.out.println("What is the name that the account should be opened under?");
-                    System.out.print("The account name should be under: ");
-                    name = Keyboard.nextLine();
-                    
-                    System.out.println("What is the address for " + name);
-                    System.out.print(name + " lives at: ");
-                    address = Keyboard.nextLine();
-                    
-                    System.out.println("What type of account should this be?");
-                    System.out.println("Valid inputs are: EVERYDAY, SAVINGS, CURRENT.");
-                    while(rightType==true){
-                        System.out.print("This account type will be: ");
-                        type = Keyboard.nextLine();
-                        type = type.toUpperCase();
-                        if(type.equals("EVERYDAY")||type.equals("SAVINGS")||type.equals("CURRENT")){
-                            rightType = false;
-                        }else{System.out.println("Bad input, please try again");}
-                    }
-                    
-                    TestPerson.infoCreator(name, address, type);
-                    errorBlock = false;
+            //This is where the bankers inputs will be used to determine what the software does next
+            if(mainInput.equals("VIEW")){
+                /**
+                 * This is where the client will be able to view all banking info
+                 */
+                System.out.println("You have selected 'view an account'.");
+                System.out.println("A full list of every account name:");
+                for(int i=0; i<allAccounts.size(); i++){
+                    System.out.println(i + ". " + allAccounts.get(i));
                 }
+            }else if(mainInput.equals("EDIT")){
+                /**
+                 * This is where the client will be able to edit pre-existing info
+                 */
+            }else if(mainInput.equals("MOVE")){
+                /**
+                 * This is where we'll be able to move money in or out of accounts
+                 */
+            }else if(mainInput.equals("OPEN")){
+                /**
+                 * This will be where new info is created using the method in CustomerInfo
+                 */
+                rightType = true;
+                System.out.println("You have selected 'open a new account'.");
+                System.out.println("What is the name that the account should be opened under?");
+                System.out.print("The account name should be under: ");
+                name = commaCheck();
+                
+                System.out.println("What is the address for " + name);
+                System.out.print(name + " lives at: ");
+                address = commaCheck();
+                
+                System.out.println("What type of account should this be?");
+                System.out.println("Valid inputs are: EVERYDAY, SAVINGS, CURRENT.");
+                while(rightType){
+                    System.out.print("This account type will be: ");
+                    type = Keyboard.nextLine();
+                    type = type.toUpperCase();
+                    if(type.equals("EVERYDAY")||type.equals("SAVINGS")||type.equals("CURRENT")){
+                        rightType = false;
+                    }else{System.out.println("Bad input, please try again");}
+                }
+            
+                TestPerson.infoCreator(name, address, type);           
+            }else if(mainInput.equals("CLOSE")){
+                /**
+                 * This is where we can delete any pre-existing accounts
+                 */
             }else if(mainInput.equals("STOP")){
                 /**
-                 * This will be where the updated information is saved to the file
+                 * This will be where the updated information is saved to the file, and the software can be closed
                  */
-                System.out.println("Changs saved to system, thank you for using my virtual banking assistance");
+                System.out.println("Changes saved to system, thank you for using the virtual banking assistant");
                 stillRunning = false;
             }else{
                 System.out.println("Bad input, please try again");
